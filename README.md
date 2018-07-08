acefb787c9a76011d997921042f9be87d983815b
Rwithik Manoj <thoriumnitride@gmail.com>
2018-07-08 14:39:20 +0530

    Minor Changes

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # song-downloader
 A Python Script to download songs in the best available format from Youtube.
+
+### Dependancies
+* `urllib`: Install `urllib` module with `pip3` with this command  
+`pip3 install urllib`
+* BeautifulSoup: Install BeautifulSoup4 with  
+`pip3 install beautifulsoup4`
+* youtube-dl: Use `pip3 install youtube_dl` to install youtube-dl
+
+### How to Use  
+* First time users please check the next section.
+* Download the downloader.py file to your system.
+* Open the terminal and into the directory where the you saved the file.
+* Run this command:  
+`python3 downloader.py`
+* It will ask you the name of the song. Enter the name.
+* Wait and it will download the song. Enjoy. ;-)
+
+### First Time Use
+* Before using the program, open it with a text editor.
+* Change `PATH GOES HERE` to the path where you want the downloaded songs to be saved. And do leave a trailing '\' character. 
+* Example: If you want them to be saved at `~/Music` folder, then line 6 shpuld look like this:  
+`pathToSave = "~/Music/"`  
+and not  
+`pathToSave = "~/Music"`
+* You're good to go. Refer How to Use if there's any issue.
--- downloader.py
+++ downloader.py
@@ -3,6 +3,8 @@ import urllib.request
 from bs4 import BeautifulSoup
 from os import system
 
+pathToSave = "PATH GOES HERE"
+
 def getSongID(song, URL):
     
     search = song + ' lyrics'
@@ -15,12 +17,15 @@ def getSongID(song, URL):
     vidID = soup.body.find_all(class_="yt-uix-tile-link")[0]['href']
     return vidID
 
-song = input("Enter the name of the song: ")
-print("Downloading " + song.title())
-URL = 'https://www.youtube.com/results?search_query='
+try:
+    song = input("Enter the name of the song: ")
+    print("Downloading " + song.title())
+    URL = 'https://www.youtube.com/results?search_query='
 
-vidID = getSongID(song, URL)
-link = 'https://www.youtube.com' + vidID
+    vidID = getSongID(song, URL)
+    link = 'https://www.youtube.com' + vidID
+    system("youtube-dl -x -q -o \'" + pathToSave + song.title() + ".%(ext)s\' \'" + link + "\'")
+    print("Downloaded " + song.title())
 
-system("youtube-dl -x -q -o \'~/Music/" + song.title() + ".%(ext)s\' \'" + link + "\'")
-print("Downloaded " + song.title())
+except:
+    print("AN ERROR OCCURED!!\nAre you connected to the internet.\nIf you are, try reading the README and see if it helps.")
