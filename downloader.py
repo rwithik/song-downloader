a58a5be4f3f0433210b0e69475cd163a5b618406
Rwithik Manoj <thoriumnitride@gmail.com>
2018-08-02 08:14:52 +0530

    Added Doc strings

--- downloader.py
+++ downloader.py
@@ -3,10 +3,30 @@ import urllib.request
 from bs4 import BeautifulSoup
 from os import system
 
+def titleCase(s):
+    '''
+    A function to convert the given song name to title case.
+    The default function in Python convert the words 'the', 'and', 'of', etc.
+    And I find that annoying
+    '''
+
+    l = s.split()
+    str = l[0][0].upper() + l[0][1:]
+    
+    for word in l[1:]:
+        if word not in ['in', 'the', 'for', 'of', 'a', 'at', 'an', 'is', 'and']:
+            str += ' ' + word[0].upper() + word[1:]
+        else:
+            str += ' ' + word
+    
+    return str
+
 pathToSave = "~/Music/"
 
-def getSongID(song, URL):
-    
+def getVidID(song, URL):
+    '''
+    This function gets the ID of the Video you have to download.
+    '''
     search = song + ' lyrics'
     searchQuery = '+'.join(search.split())
     searchURL = URL + searchQuery
@@ -28,10 +48,10 @@ for i in range(n):
 for song in songList:
     try:
         # song = input("Enter the name of the song: ")
-        print("Downloading " + song.title())
+        print("Downloading " + song.titleCase())
         URL = 'https://www.youtube.com/results?search_query='
     
-        vidID = getSongID(song, URL)
+        vidID = getVidID(song, URL)
         link = 'https://www.youtube.com' + vidID
         system("youtube-dl -x -q -o \'" + pathToSave + song.title() + ".%(ext)s\' \'" + link + "\'")
         print("Downloaded " + song.title() + "\n")
