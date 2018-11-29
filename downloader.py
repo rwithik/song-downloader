26eb3b408c8d4094a5e1fb2223e64996a5c6a5d7
rwithik <rwithik@gmail.com>
2018-11-29 15:06:31 +0530

    minor edits

--- downloader.py
+++ downloader.py
@@ -5,12 +5,6 @@ from os import system
 import sys
 
 def titleCase(s):
-    '''
-    A function to convert the given song name to title case.
-    The default function in Python convert the words 'the', 'and', 'of', etc.
-    And I find that annoying
-    '''
-
     l = s.split()
     str = l[0][0].upper() + l[0][1:]
     list = ['in', 'the', 'for', 'of', 'a', 'at', 'an', 'is', 'and', 'to']
@@ -19,46 +13,28 @@ def titleCase(s):
             str += ' ' + word[0].upper() + word[1:]
         else:
             str += ' ' + word
-    
+
     return str
 
-pathToSave = "~/Music/"
-
 def getVidID(song, URL):
-    '''
-    This function gets the ID of the Video you have to download.
-    '''
     search = song + ' lyrics'
     searchQuery = '+'.join(search.split())
     searchURL = URL + searchQuery
-    
+
     response = urllib.request.urlopen(searchURL)
     soup = BeautifulSoup(response.read(), "lxml")
 
     vidID = soup.body.find_all(class_="yt-uix-tile-link")[0]['href']
     return vidID
 
-#songList = []
-#n = int(input("Enter the number of songs: "))
-#print('Enter the song names')
-#i =1
-#for i in range(n):
-#    song = input()
-#    songList = songList + [song]
-
-try:
-    print('-------------------------------------------------------------')
-    for song in sys.argv[1:]:
-        # song = input("Enter the name of the song: ")
-        print("Downloading " + titleCase(song))
-        URL = 'https://www.youtube.com/results?search_query='
-        vidID = getVidID(song, URL)
-        link = 'https://www.youtube.com' + vidID
-        system("youtube-dl -x -q -o \'" + pathToSave + titleCase(song) + ".%(ext)s\' \'" + link + "\'")
-        print("Downloaded " + titleCase(song) + "\n") 
-        # print(link)
-
-except:
-    print("AN ERROR OCCURED!!\nAre you connected to the internet?\nIf you are, try reading the README and see if it helps.")
-
-
+print('-------------------------------------------------------------')
+pathToSave = "Music/"
+for song in sys.argv[1:]:
+    # song = input("Enter the name of the song: ")
+    print("Downloading " + titleCase(song))
+    URL = 'https://www.youtube.com/results?search_query='
+    vidID = getVidID(song, URL)
+    link = 'https://www.youtube.com' + vidID
+    system("youtube-dl -q -x -o \'" + pathToSave + titleCase(song) + ".%(ext)s\' \'" + link + "\'")
+    print("Downloaded " + titleCase(song) + "\n")
+    # print(link)
