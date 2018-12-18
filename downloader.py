a703ee74ff626d182d2c04f31189b9f723745254
rwithik <rwithik@gmail.com>
2018-12-18 16:00:29 +0530

    fixed

--- downloader.py
+++ downloader.py
@@ -2,6 +2,7 @@ import urllib
 import urllib.request
 from bs4 import BeautifulSoup
 from os import system
+import sys
 
 def titleCase(s):
     '''
@@ -37,27 +38,16 @@ def getVidID(song, URL):
     vidID = soup.body.find_all(class_="yt-uix-tile-link")[0]['href']
     return vidID
 
-songList = []
-n = int(input("Enter the number of songs: "))
-print('Enter the song names')
-i =1
-for i in range(n):
-    song = input()
-    songList = songList + [song]
-
-#try:
+try:
 print('-------------------------------------------------------------')
-for song in songList:
-    # song = input("Enter the name of the song: ")
+for song in sys.argv[1:]:
     print("Downloading " + titleCase(song))
     URL = 'https://www.youtube.com/results?search_query='
     vidID = getVidID(song, URL)
     link = 'https://www.youtube.com' + vidID
     system("youtube-dl -x -q -o \'" + pathToSave + titleCase(song) + ".%(ext)s\' \'" + link + "\'")
     print("Downloaded " + titleCase(song) + "\n") 
-        # print(link)
-
-
-#    print("AN ERROR OCCURED!!\nAre you connected to the internet?\nIf you are, try reading the README and see if it helps.")
+except:
+    print("AN ERROR OCCURED!!\nAre you connected to the internet?\nIf you are, try reading the README and see if it helps.")
 
 
